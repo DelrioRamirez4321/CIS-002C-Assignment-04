3e377274f830d5024842aa216fb9eb66b4f674ae
DelrioRamirez4321 <delrioramirez7813@gmail.com>
2020-02-06 16:52:25 -0800

    finished questions 3

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // console.log(min(0, 10)); // will output 0.
 // console.log(min(0, -10)); // will output -10
 
-let min = function(n, n2) {
-  if (n >= n2) {
-    return n2;
-  }
+let min = function (n, n2) {
+    if (n >= n2) {
+        return n2;
+    }
     return n;
 };
 
@@ -17,29 +17,29 @@ console.log(min(0, -10));
 // Example Output:
 // console.log(calculateCircumference(5)); // will output 31.4
 
-let calculateCircumference = function(n) {
+let calculateCircumference = function (n) {
     let pi = 3.14;
 
     return 2 * pi * n;
 
 };
 
-console.log(calculateCircumference(5)); 
+console.log(calculateCircumference(5));
 
 // 3. Write a function searchString that takes a character and then returns how many times it appears in another string argument.
 // Example Output:
 // console.log(searchString(“s”, “mississippi”)); // will output 4
 // console.log(searchString(“q”, “mississippi”)); // will output 0
 
-let searchString = function(chr, str) {
+let searchString = function (chr, str) {
     let numberOfCharactersFound = 0;
 
-    while (i = 0, i <= str.length -1, i++) {
+    for (i = 0; i < str.length; i++) {
         if (str[i] == chr) {
             numberOfCharactersFound++
-        }
-        return numberOfCharactersFound;
-    }
+        };
+    };
+    return numberOfCharactersFound;
 };
 
 console.log(searchString("s", "mississippi"));
